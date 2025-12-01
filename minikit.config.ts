e2d2d86b2d95bcf35a46244445a093736c906ec3
NonFungibleH <howardpearce@live.co.uk>
2025-12-02 02:15:55 +0700

    Update minikit.config.ts

--- minikit.config.ts
+++ minikit.config.ts
@@ -7,8 +7,11 @@ export const manifest = {
   miniAppDescription: 'Create your own token on Base in seconds',
   miniAppIconUrl: `${process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'}/icon.png`,
   accountAssociation: {
-    header: 'eyJmaWQiOjAsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwIn0',
-    payload: 'eyJkb21haW4iOiJleGFtcGxlLmNvbSJ9',
-    signature: 'MHgwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDA',
+    header: 'eyJmaWQiOjU1ODY5NCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDI5OTRlMjIyZjRiNzVjMTUzMjJFODg4QTZjODUyOTcxNjkwNDM3OWIifQ',
+    payload: 'eyJkb21haW4iOiJsYXVuY2gtbWluaS1hcHAtc3FncC52ZXJjZWwuYXBwIn0',
+    signature: 'JZsh6ohlMM74K5t7i7e2/VZ/nv26mGuxFcNbzlM8lklOo3j9rhOccF1B98YwK7OBWilOeTK7j4ZPTaHmiLKv/Rw=',
+  },
+  baseBuilder: {
+    ownerAddress: '0xC885476F424F92717C0218031b896513828a2ab7',
   },
 };

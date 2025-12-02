12578d266c02e0acb60350aceb5d131fb20918a8
NonFungibleH <howardpearce@live.co.uk>
2025-12-02 23:00:39 +0700

    Update minikit.config.ts

--- minikit.config.ts
+++ minikit.config.ts
@@ -1,17 +1,10 @@
-export const manifest = {
-  miniAppName: 'Launch',
-  splashImageUrl: `${process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'}/splash.png`,
-  splashBackgroundColor: '#0052FF',
-  homeUrl: process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000',
-  imageUrl: `${process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'}/icon.png`,
-  miniAppDescription: 'Create your own token',
-  miniAppIconUrl: `${process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'}/icon.png`,
-  accountAssociation: {
-    header: 'eyJmaWQiOjU1ODY5NCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDI5OTRlMjIyZjRiNzVjMTUzMjJFODg4QTZjODUyOTcxNjkwNDM3OWIifQ',
-    payload: 'eyJkb21haW4iOiJsYXVuY2gtbWluaS1hcHAtc3FncC52ZXJjZWwuYXBwIn0',
-    signature: 'JZsh6ohlMM74K5t7i7e2/VZ/nv26mGuxFcNbzlM8lklOo3j9rhOccF1B98YwK7OBWilOeTK7j4ZPTaHmiLKv/Rw=',
-  },
-  baseBuilder: {
-    ownerAddress: '0xC885476F424F92717C0218031b896513828a2ab7',
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
   },
 };
+
+export default nextConfig;

6300fcbd5586d4842af4b15fb19083c312bd5526
NonFungibleH <howardpearce@live.co.uk>
2025-12-02 22:06:45 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding');
-    return config;
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/019adf62-611f-736b-a6d9-2019d7f59425',
+        permanent: false,
+        statusCode: 307,
+      },
+    ]
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
